b503ff4b153be5330108c9612a4c6bd300e1efb0
JunhaoHo <122047397@qq.com>
2015-11-17 00:17:33 +0800

    调整图像压缩比

--- client/js/bubble.js
+++ client/js/bubble.js
@@ -331,19 +331,19 @@ $(function() {
 	        var category = $(this).data('category');
 	        var to = $(this).data('room');
 	        var from = $("#bubble_username").val();
-	        var IMAGE_LIMIT_SIZE = 1.5*1024*1024;
+	        var IMAGE_LIMIT_SIZE = 1.0*1024*1024;
 	        var imgData = '';
 	        FR.onload = function(e) {
 	        		if ($.inArray( currfile.type, mime )==-1){
 	        			swal('非法文件', '只支持发送png、jpg、bmp和gif类型的图片', 'warning');
 	        			console.debug(currfile.type);
-	        		} else if (currfile.size > IMAGE_LIMIT_SIZE) { //大于1.5MB需要压缩
+	        		} else if (currfile.size > IMAGE_LIMIT_SIZE) { //大于1MB需要压缩
 	        			waitingDialog.show('正在发送...', {
             				dialogSize: 'sm',
             				progressType: 'success',
             				onHide:function(){}
 					});
-	        			lrz(currfile, {quality:0.6})
+	        			lrz(currfile, {quality:0.4})
 				    .then(function (rst) {
 				    		console.debug('压缩前大小：'+currfile.size/1024+'KB');
 				    		console.debug('压缩后大小：'+rst.fileLen/1024+'KB'+'   Base64Len：'+rst.base64Len/1024+'KB');
